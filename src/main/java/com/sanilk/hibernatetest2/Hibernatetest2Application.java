986f31508e3cf9194efe9465c39ca9fae3b31520
Sanil Khurana <sanilkhurana7@gmail.com>
2018-11-02 14:24:30 +0530

    First commit:

---
+++ src/main/java/com/sanilk/hibernatetest2/Hibernatetest2Application.java
@@ -0,0 +1,12 @@
+package com.sanilk.hibernatetest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernatetest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hibernatetest2Application.class, args);
+    }
+}
